20da01367fb43901b56fb93460727a660b9f6ab0
Yuri Gu <yurigu@Yuriui-MacBookPro.local>
2019-07-30 17:30:46 +0900

    route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Header } from "./pages/header.js";
+// import { DetailPage } from "./pages/detail.js";
+// import { Footer } from "./pages/footer.js";
+
+// function Header() {
+//   return (
+//     <div>
+//       <h2>Header</h2>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router path="/">
+      <Route path="/" component={Header} />
+      {/* <Route component={DetailPage} />
+      <Route component={Footer} /> */}
+    </Router>
   );
 }
 
