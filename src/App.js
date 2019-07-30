16904b07b37d73d9f283c3ff93b7e13f4a584e5b
Yuri Gu <yurigu@Yuriui-MacBookPro.local>
2019-07-30 18:23:14 +0900

    route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Route exact path="/" component={Home} />
       <Route exact path="/" component={DetailPage} />
-      {/* <Route exact path="/" component={Footer} /> */}
+      <Route exact path="/" component={Footer} />
     </BrowserRouter>
   );
 }
